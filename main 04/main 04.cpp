133effc4d0df67c3a8dbb1cb8f63ee16bc11df74
user <user@DESKTOP-TNL7I4L>
2025-11-24 14:33:51 +0600

    r

--- main 04/main 04.cpp
+++ main 04/main 04.cpp
@@ -1,19 +1,6 @@
 ﻿#include <iostream>
 using namespace std;
 
-void task1();
-void task2();
-void task3();
-void task4();
-void task5();
-void task6();
-void task7();
-void task8();
-void task9();
-void task10();
-void task11();
-
-
 
 void task1() {
     int n;
