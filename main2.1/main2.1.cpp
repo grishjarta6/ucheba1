d28f88fa6d7c416197a30f29204bb236436d2d72
user <user@DESKTOP-TNL7I4L>
2026-02-06 11:39:46 +0600

    vb

--- main2.1/main2.1.cpp
+++ main2.1/main2.1.cpp
@@ -18,10 +18,39 @@ void ts1() {
     cout << fac(x);
 }
 
+int cd(int n) {
+    if (n < 10) {
+        return 1;
+    }
+    else {
+        return 1 + cd(n / 10);
+    }
+}
+
 void ts2() {
+    int x;
+    cin >> x;
+    cout << cd(x);
+}
+
+int sd(int n) {
+    if (n < 10) {
+        return n;
+    }
+    else {
+        return (n % 10) + sd(n / 10);
+    }
+}
+
+void ts3() {
+    int x;
+    cin >> x;
+    cout << sd(x);
 
 }
 
 int main() {
     //ts1();
+    //ts2();
+    ts3();
 }
