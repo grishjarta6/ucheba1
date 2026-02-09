314b9543f4851bf80c5d055d05bd83eb203fb2bd
user <user@DESKTOP-TNL7I4L>
2026-02-09 13:48:56 +0600

    rrr

--- main2.1/main2.1.cpp
+++ main2.1/main2.1.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 #include <cmath>
 #include <iomanip>
+#include <string>
 using namespace std;
 
 int fac(int n) {
@@ -49,8 +50,90 @@ void ts3() {
 
 }
 
+int ts41(int n) {
+    if (n < 10) {
+        return n;
+    }
+    else {
+        return (n % 10) + sd(n / 10);
+    }
+}
+
+int ts42(int a) {
+    int s;
+    s = ts41(a);
+    if (s < 10) { return s;}
+    else {
+        return ts41(s);
+    }
+}
+
+void ts4() {
+    int x;
+    cin >> x;
+    cout << ts42(x);
+}
+
+void ts5() {
+    //я не понял
+}
+
+int ts61(int a, int b) {
+    b += 1;
+    if ((a % b == 0) and (b != a)) {
+        return 0;
+    }
+    if (a == b) {
+        return 1;
+    }
+    else {
+        ts61(a, b);
+    }
+}
+
+void ts6() {
+    int x, q = 1, a;
+    cin >> x;
+    a = ts61(x, q);
+    if (a == 1) { cout << "yes"; }
+    else {cout << "no";}
+}
+
+int ts71(int n) {
+    if (n < 10) { return n; }
+    else {
+        
+        cout << n % 2;
+        return ts71(n / 10);
+    }
+}
+
+int b(int b) {
+    if (b == 0) { return b; }
+    b
+}
+
+void f() {
+    int n;
+    cin >> n;
+    if (n == 0) { return ; }
+    f();
+    cout << n;
+}
+
+void ts7() {
+    int x;
+    cin >> x;
+    cout << ts71(x);
+}
+
 int main() {
     //ts1();
     //ts2();
-    ts3();
+    //ts3();
+    //ts4();
+
+    //ts6();
+    //ts7();
+    //f();
 }
