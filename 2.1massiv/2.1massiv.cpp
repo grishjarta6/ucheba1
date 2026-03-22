115b8a56e9c428e5ca0eee9cc078a210e9acb276
user <user@DESKTOP-4N2N0M0>
2026-03-22 16:50:18 +0300

    ff

--- 2.1massiv/2.1massiv.cpp
+++ 2.1massiv/2.1massiv.cpp
@@ -150,6 +150,220 @@ void ts1_7() {
 	}
 }
 
+// двумерного массива/матрицы
+
+void ts2_1() {
+	int n, x, y;
+	cin >> n;
+	int** m = new int* [n];
+	for (x = 0; x < n; x += 1) {
+		m[x] = new int[n];  
+	}
+	for (x = 0; x < n; x += 1) {
+		for (y = 0; y < n; y += 1) {
+			m[x][y] = x + 1;
+		}
+	}
+	for (x = 0; x < n; x += 1) {
+		for (y = 0; y < n; y += 1) {
+			cout << m[x][y];
+		}
+		cout << endl;
+	}
+	for (x = 0; x < n; x += 1) { delete[] m[x]; }
+	delete[] m;
+}
+
+void ts2_2() {
+	int n, m, x, y, xs, ys;
+	cin >> n >> m;
+	cout << endl;
+	int** ar = new int* [n];
+
+	for (x = 0; x < n; x += 1) { ar[x] = new int[m]; }
+
+	for (x = 0; x < n; x += 1) {
+		for (y = 0; y < m; y += 1) {
+			xs = x - 1;
+			ys = y - 1;
+			if ((xs < 0) or (ys < 0)) {
+				ar[x][y] = 1;
+			}
+			else {
+				xs = ar[x - 1][y];
+				ys = ar[x][y - 1];
+				ar[x][y] = xs + ys;
+			}
+		}
+	}
+	for (x = 0; x < n; x += 1) {
+		for (y = 0; y < m; y += 1) {
+			cout << ar[x][y] << " ";
+		}
+		cout << endl;
+	}
+	for (x = 0; x < n; x += 1) {delete[] ar[x];}
+	delete[] ar;
+}
+
+void ts2_3() {
+	int n, x, y;
+	cin >> n;
+	int** m = new int* [n];
+	for (x = 0; x < n; x += 1) { m[x] = new int[n]; }
+	for (x = 0; x < n; x += 1) {
+		for (y = 0; y < n; y += 1) {
+			if (x % 2 == 0) {
+				if (y % 2 == 0) {
+					m[x][y] = 1;
+				}
+				else {
+					m[x][y] = 0;
+				}
+			}
+			else {
+				if (y % 2 != 0) {
+					m[x][y] = 1;
+				}
+				else {
+					m[x][y] = 0;
+				}
+			}
+		}
+	}
+	for (x = 0; x < n; x += 1) {
+		for (y = 0; y < n; y += 1) {
+			cout << m[x][y];
+		}
+		cout << endl;
+	}
+	for (x = 0; x < n; x += 1) { delete[] m[x]; }
+	delete[] m;
+
+}
+
+void ts2_4() {
+	int n, m, x, y, c = 1;
+	cin >> n >> m;
+	cout << endl;
+	int** ar = new int* [n];
+
+	for (x = 0; x < n; x += 1) { ar[x] = new int[m]; }
+
+	for (x = 0; x < n; x += 1) {
+		for (y = 0; y < m; y += 1) {
+			ar[x][y] = c;
+			c += 1;
+		}
+	}
+	for (x = 0; x < n; x += 1) {
+		for (y = 0; y < m; y += 1) {
+			cout << ar[x][y] << " ";
+		}
+		cout << endl;
+	}
+	for (x = 0; x < n; x += 1) { delete[] ar[x]; }
+	delete[] ar;
+}
+
+void ts2_5() {
+	int n, x, y, i, c = 0, ci, yi, p;
+	cin >> n;
+	int** m = new int* [n];
+	for (x = 0; x < n; x += 1) { m[x] = new int[n]; }
+	for (x = 0; x < n; x += 1) {
+		for (y = 0; y < n; y += 1) {
+			m[x][y] = y + 1;
+		}
+		p = n;
+		for (i = 0; i < c; i++) {
+			ci = m[x][0];
+			//cout << ci << endl;
+			for (yi = 1; yi < p; yi += 1) {
+				m[x][yi - 1] = m[x][yi];
+				p - 1;
+			}
+			m[x][n - 1] = ci;
+		}
+		c += 1;
+	}
+	for (x = 0; x < n; x += 1) {
+		for (y = 0; y < n; y += 1) {
+			cout << m[x][y] << " ";
+		}
+		cout << endl;
+	}
+}
+
+void ts2_6() {
+	int n, m, x, y;
+	cin >> n >> m;
+	cout << endl;
+	int** ar = new int* [n];
+
+	for (x = 0; x < n; x += 1) { ar[x] = new int[m]; }
+
+	for (x = 0; x < n; x += 1) {
+		for (y = 0; y < m; y += 1) {
+			ar[x][y] = (x * y);
+		}
+	}
+	x = 0;
+	y = 0;
+	for (x = 0; x < n; x += 1) {
+		for (y = 0; y < n; y += 1) {
+			cout << ar[x][y] << " ";
+		}
+		cout << endl;
+	}
+}
+
+void ts2_7() {
+	int n, x, y, c = 1, cx = 0, cy = 0;
+	cin >> n;
+	int** m = new int* [n];
+	for (x = 0; x < n; x += 1) { m[x] = new int [n]; }
+
+	for (x = 0; x < n; x += 1) {
+		for (y = 0; y < n; y += 1) {
+			m[x][y] = c;
+			c += 1;
+		}
+	}
+	for (x = 0; x < n; x += 1) {
+		for (y = 0; y < n; y += 1) {
+			if (x == y) {
+				cout << m[x][y] << " ";
+			}
+		}
+	}
+}
+
+void ts2_8() {
+	int n, x, y, c = 1, cx = 0, cy = 0;
+	cin >> n;
+	int** m = new int* [n];
+	for (x = 0; x < n; x += 1) { m[x] = new int[n]; }
+
+	for (x = 0; x < n; x += 1) {
+		for (y = 0; y < n; y += 1) {
+			m[x][y] = c;
+			c += 1;
+		}
+	}
+	cy = n - 1;
+	for (x = 0; x < n; x += 1) {
+		for (y = 0; y < n; y += 1) {
+			if (y == cy) {
+				cout << m[x][y] << " ";
+			}
+		}
+		cy -= 1;
+	}
+}
+
+
+
 int main() {
 	//ts1_1();
 	//ts1_2();
@@ -158,4 +372,14 @@ int main() {
 	//ts1_5();
 	//ts1_6();
 	//ts1_7();
+
+	//ts2_1();
+	//ts2_2();
+	//ts2_3();
+	//ts2_4();
+	//ts2_5();
+	//ts2_6();
+	//ts2_7();
+	//ts2_8();
+
 }
