a714ded2e527f3ee3e8f689b53f8643377284d85
Arseniy laptop <error654arseniy@gmail.com>
2026-04-27 16:52:10 +0300

    аа

---
+++ 2.4sort/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void ts1(){
+    int n, x, i, j;
+    cin >> n;
+    int* m = new int [n];
+    for (i = 0; i <= n; i += 1){
+        cin >> x;
+        m[i] = x;
+    }
+
+    
+    
+}
+
+int main(){
+
+    return 0;
+}
