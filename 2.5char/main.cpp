edf58e749648272174d11108d1ff90c0792d5e2a
Arseniy laptop <error654arseniy@gmail.com>
2026-05-13 17:16:15 +0300

    ff

--- 2.5char/main.cpp
+++ 2.5char/main.cpp
@@ -25,17 +25,52 @@ void ts2(){
 
 void ts3(){
     const int m = 500;
-    char n[500], n1[500], n2[500];
-    int i;
-    cin.getline(n, m)
-    
-    
+    char n[500];                
+    cin.getline(n, m);    
 
+    int pos = -1;
+    for (int i = 0; n[i] != '\0'; ++i) {
+        if (n[i] == ' ') {
+            pos = i;
+            break;
+        }
+    }
+
+    cout << (n + pos + 1) << ' ';
+    for (int i = 0; i < pos; ++i) {
+        cout << n[i];
+    }
+    cout << endl;
 }
+
+void ts4(){
+    string s;
+    getline(cin, s);
+
+    int count_f = 0;
+    int second_index = -1;
+
+    for (int i = 0; i < (int)s.size(); ++i) {
+        if (s[i] == 'f') {
+            count_f++;
+            if (count_f == 2) {
+                second_index = i;
+                break;
+            }
+        }
+    }
+
+    if (count_f == 0) cout << -2 << endl;
+    else if (count_f == 1) cout << -1 << endl;
+    else cout << second_index << endl;
+}
+
+
 
 int main(){
     //ts1();
     //ts2();
-    ts3();
+    //ts3();
+    //ts4();
     return 0;
 }
