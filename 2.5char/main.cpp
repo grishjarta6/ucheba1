ccb86a970255040dceed60a6687201f17e3255bb
Arseniy laptop <error654arseniy@gmail.com>
2026-05-15 12:33:30 +0300

    уу

--- 2.5char/main.cpp
+++ 2.5char/main.cpp
@@ -1,3 +1,6 @@
+#include <cstddef>
+#include <cstdio>
+#include <iomanip>  // для setprecision
 #include <iostream>
 #include <string>
 
@@ -65,12 +68,73 @@ void ts4(){
     else cout << second_index << endl;
 }
 
+void ts5(){
+    int n, i;
+    char c;
+    bool zb = false, sb = false, ch = false;
+    string s; 
+    getline(cin, s);
+    n = s.size();
+    if (n >= 8){
+        for (i = 0; i <= n; i += 1){
+            c = s[i];
+            if ((c >= 'a') and (c <='z')){sb = true;}
+            if ((c >= 'A') and (c <='Z')){zb = true;}
+            if ((c >= '0') and (c <='9')){ch = true;}
+        }
+    }
+    //else {cout << "no" << endl;}
+    if (zb and sb and ch){cout << "yes" << endl;}
+    else {cout << "no" << endl;}
+}
 
+void ts6(){
+    int i, n;
+    string s;
+    char c, l;
+
+    getline(cin, s);
+    n = s.size();
+    l = s[n - 1];
+    cout << l << endl;
+    if (l == 'l'){
+        for (i = 0; i < n - 1; i += 1){
+            c = s[i];
+            cout << c;
+        }
+        cout << endl;
+    }
+    else{cout << s << endl;}
+}
+
+void ts7(){
+    int n;
+    char t;
+    string s;
+    cin >> n >> t;       
+    cin >> s;                 
+
+    int c = 0;
+    for (int i = 0; i < (int)s.size(); ++i) {
+        if (s[i] == t) {
+            c+=1;
+        }
+    }
+
+    double freq = (double)c / n;
+    cout << fixed << setprecision(6) << freq << endl;
+    //10 a
+    //abbacadaff
+    
+}
 
 int main(){
     //ts1();
     //ts2();
     //ts3();
     //ts4();
+    //ts5();
+    //ts6();
+    //ts7();
     return 0;
 }
